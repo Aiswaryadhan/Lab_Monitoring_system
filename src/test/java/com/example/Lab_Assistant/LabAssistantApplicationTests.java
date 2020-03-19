0b5076d5c04da4a70d466609a82e6293c1e8d536
Aiswaryadhan <aisu1495@gmail.com>
2020-03-20 00:14:59 +0530

    First Commit

---
+++ src/test/java/com/example/Lab_Assistant/LabAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab_Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
