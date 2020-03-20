446e08af0c2ccd5af6a71939bafd92ba5939ffc4
Aiswaryadhan <aisu1495@gmail.com>
2020-03-20 21:05:38 +0530

    added jpa dependencies

--- src/main/java/com/example/Lab_Assistant/db/models/repositories/TeacherRepository.java
+++ src/main/java/com/example/Lab_Assistant/db/models/repositories/TeacherRepository.java
@@ -4,4 +4,5 @@ import com.example.Lab_Assistant.db.models.Teacher;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TeacherRepository extends CrudRepository<Teacher,String> {
+    Teacher findOne(String studentID);
 }
