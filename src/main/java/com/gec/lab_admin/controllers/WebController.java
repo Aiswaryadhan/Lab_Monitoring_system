b0e89863366b3b3bbd65d369ca2145fd75abaa76
Aiswaryadhan <aisu1495@gmail.com>
2020-03-21 12:19:41 +0530

    sample web controller

--- src/main/java/com/gec/lab_admin/controllers/WebController.java
+++ src/main/java/com/gec/lab_admin/controllers/WebController.java
@@ -1,4 +1,25 @@
 package com.gec.lab_admin.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class WebController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Welcome";
+    }
+    @RequestMapping("/hello")
+    public String welcome(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/teacher_login.html")
+    public String login() {
+        return "teacher_login.html";
+    }
+
 }
